aef72f79ef795ca8e5d97e25479d813d44976b12
景 <1946617458@qq.com>
2023-03-30 16:34:12 +0800

    初始化项目

---
+++ mall-member/src/test/java/com/jing/mall/member/MallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jing.mall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
